d69bdfcbb6f2a3d7d9750d7e6734a7d1cf077389
JulienLuxx <JulienLux@outlook.com>
2019-09-16 10:47:20 +0800

    AddServiceCollectionExtensions

---
+++ Common/Common.CoreUtil/ServiceCollectionExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Common.CoreUtil
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddHttpClientUtil(this IServiceCollection services)
+        {
+            services.AddScoped<IHttpClientUtil, HttpClientUtil>();
+            return services;
+        }
+    }
+}
---
+++ Common/Common.Util/ServiceCollectionExtensions.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Common.Util
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddMapUtil(this IServiceCollection services)
+        {
+            services.AddScoped<IMapUtil, MapUtil>();
+            return services;
+        }
+
+        public static IServiceCollection AddEncryptUtil(this IServiceCollection services)
+        {
+            services.AddScoped<IEncryptUtil, EncryptUtil>();
+            return services;
+        }
+    }
+}
