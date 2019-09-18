d031ea4f39ceb42d9c8c1ba50f3a01a9c1fa1205
JulienLuxx <JulienLux@outlook.com>
2019-09-18 20:19:02 +0800

    Tmp

--- Common/Common.CoreUtil/IHttpClientUtil.cs
+++ Common/Common.CoreUtil/IHttpClientUtil.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Common.Util;
+using System;
 using System.Collections.Generic;
 using System.Net.Http;
 using System.Text;
@@ -6,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace Common.CoreUtil
 {
-    public interface IHttpClientUtil
+    public interface IHttpClientUtil: IDependency
     {
         /// <summary>
         /// AsyncSendPackage
