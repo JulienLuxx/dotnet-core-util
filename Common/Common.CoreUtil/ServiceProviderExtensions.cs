92803d4a7e0f3b4a5f3b8fad1f27866ce412aa6d
JulienLuxx <JulienLux@outlook.com>
2019-12-26 11:13:56 +0800

    AddServiceProviderExtensionFunc

---
+++ Common/Common.CoreUtil/ServiceProviderExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace Common.CoreUtil
+{
+    public static class ServiceProviderExtensions
+    {
+        /// <summary>
+        /// Get HttpClientUtil from IServiceProvider
+        /// </summary>
+        /// <param name="provider"></param>
+        /// <returns></returns>
+        public static IHttpClientUtil GetHttpClientUtilService(this IServiceProvider provider)
+        {
+            return provider.GetService<IHttpClientUtil>();
+        }
+    }
+}
