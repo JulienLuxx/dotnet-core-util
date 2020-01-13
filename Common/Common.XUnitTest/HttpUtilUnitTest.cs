335dfb37fe94d16da86819c70fafcf28d552ebb4
JulienLuxx <JulienLux@outlook.com>
2020-01-13 15:53:44 +0800

    TrySameUnitTest

--- Common/Common.XUnitTest/HttpUtilUnitTest.cs
+++ Common/Common.XUnitTest/HttpUtilUnitTest.cs
@@ -6,9 +6,66 @@ using Microsoft.Extensions.DependencyInjection;
 using System.Threading.Tasks;
 using Xunit;
 using System.IO;
+using System.ComponentModel;
+using Newtonsoft.Json;
 
 namespace Common.XUnitTest
 {
+    public class OrganParam
+    {
+        //[JsonPropertyName("organ_code")]
+        [JsonProperty("data.organ_code")]
+        [Description("data.organ_code")]
+        public string OrganCode { get; set; }
+
+        //[JsonPropertyName("organ_name")]
+        [JsonProperty("data.organ_name")]
+        [Description("data.organ_name")]
+        public string OrganName { get; set; }
+
+        //[JsonPropertyName("short_name")]
+        [JsonProperty("data.short_name")]
+        [Description("data.short_name")]
+        public string ShortName { get; set; }
+
+        //[JsonPropertyName("parent_organ_code")]
+        [JsonProperty("data.parent_organ_code")]
+        [Description("data.parent_organ_code")]
+        public string ParentOrganCode { get; set; }
+
+        //public OrganParam(OrganArg arg, IMapper mapper)
+        //{
+        //    mapper.Map(arg, this);
+        //}
+
+        public override string ToString()
+        {
+            return JsonConvert.SerializeObject(this);
+        }
+    }
+
+    public class UserCenterSendParam
+    {
+
+        [Description("data.organ_code")]
+        public string OrganCode { get; set; }
+
+        [Description("data.organ_name")]
+        public string OrganName { get; set; }
+
+        [Description("data.short_name")]
+        public string ShortName { get; set; }
+
+        [Description("data.parent_organ_code")]
+        public string ParentOrganCode { get; set; }
+
+        [Description("appCode")]
+        public string AppCode { get; set; }
+
+        [Description("appToken")]
+        public string AppToken { get; set; }
+    }
+
     public class HttpUtilUnitTest : BaseUnitTest
     {
         private IHttpClientUtil _httpClientUtil { get; set; }
@@ -45,10 +102,24 @@ namespace Common.XUnitTest
             return httpResult;
         }
 
+        public async Task<HttpResult> SendAsync3()
+        {
+            var param = new UserCenterSendParam()
+            {
+                OrganCode = "636",
+                OrganName = "Test",
+                ParentOrganCode = "456",
+                AppCode = "233",
+                AppToken = "2333"
+            };
+            var httpResult = await _httpClientUtil.SendAsync(param, @"http://localhost:5000/API/Organ/AddOrgan", "POST", MediaTypeEnum.ApplicationFormUrlencoded);
+            return httpResult;
+        }
+
         [Fact]
         public async Task SendAsyncGenericTest()
         {
-            var result = await SendAsync2();
+            var result = await SendAsync3();
             Assert.True(result.IsSuccess);
         }
 
