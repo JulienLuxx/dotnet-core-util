4c748eb7646cfee46e7c032fad7ff79739f24ca7
JulienLuxx <JulienLux@outlook.com>
2020-03-04 21:58:19 +0800

    AddEnumExtensionsFunc&AddUnitTest

---
+++ Common/Common.Util/EnumExtensions.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace Common.Util
+{
+    public static class EnumExtensions
+    {
+        public static string GetDescription(this Enum instance)
+        {
+            var type = instance.GetType();            
+            if (null == type)
+            {
+                return string.Empty;
+            }
+            var name = GetName(type, instance);
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                return string.Empty;
+            }
+            var member = type.GetTypeInfo().GetMember(name).FirstOrDefault();
+            if (null == member)
+            {
+                return string.Empty;
+            }
+            return member.GetCustomAttribute<DescriptionAttribute>() is DescriptionAttribute attribute ? attribute.Description : member.Name;
+        }
+
+        public static string GetName(Type type, dynamic obj)
+        {
+            if (null == type)
+            {
+                return string.Empty;
+            }
+            if (null == obj)
+            {
+                return string.Empty;
+            }
+            if (obj is string)
+            {
+                obj.ToString();
+            }
+            if (type.GetTypeInfo().IsEnum == false)
+            {
+                return string.Empty;
+            }
+            return Enum.GetName(type, obj);
+        }
+    }
+}
---
+++ Common/Common.XUnitTest/EnumExtensionsUnitTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using Common.Util;
+using Xunit;
+
+namespace Common.XUnitTest
+{
+    public enum TestEnum
+    {
+        [Description("Unable")]
+        Unable=-1,
+
+        [Description("Default")]
+        Default,
+
+
+        [Description("Activate")]
+        Activate,
+    }
+
+    public class EnumExtensionsUnitTest
+    {
+        [Fact]
+        public void GetDescriptionTest()
+        {
+            var name = TestEnum.Activate.GetDescription();
+            Assert.Equal("Activate", name);
+        }
+    }
+}
