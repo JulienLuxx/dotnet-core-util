454193061a7d71086715f4b875e2c22b76ce24d5
JulienLuxx <JulienLux@outlook.com>
2020-10-21 15:02:11 +0800

    AddRuntimeHelper

---
+++ Common/Common.Util/Helper/RuntimeHelper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace Common.Util
+{
+    public static class RuntimeHelper
+    {
+        public static bool IsWindowRunTime()
+        {
+            return RuntimeInformation.IsOSPlatform(OSPlatform.Windows);
+        }
+
+        public static bool IsLinuxRunTime()
+        {
+            return RuntimeInformation.IsOSPlatform(OSPlatform.Linux);
+        }
+    }
+}
