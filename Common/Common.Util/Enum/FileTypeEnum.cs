f2187e6f739941bf7f61cb1e7d5d1a199f670074
JulienLuxx <JulienLux@outlook.com>
2020-10-21 11:46:45 +0800

    AddFileTypeEnum

---
+++ Common/Common.Util/Enum/FileTypeEnum.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace Common.Util
+{
+    public enum FileType
+    {
+        [Description("Audio")]
+        Audio,
+
+        [Description("Image")]
+        Image,
+
+        [Description("Excel")]
+        Excel,
+
+        [Description("PDF")]
+        PDF,
+
+        [Description("PPT")]
+        PPT,
+
+        [Description("Text")]
+        Text,
+
+        [Description("Video")]
+        Video,
+
+        [Description("WindowsExecutable")]
+        WindowsExecutable,
+
+        [Description("Word")]
+        Word,
+
+        [Description("Zip")]
+        Zip,
+
+        [Description("UnSupport")]
+        UnSupport=-1
+    }
+}
