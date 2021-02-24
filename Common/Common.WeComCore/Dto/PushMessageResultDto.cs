e33326b89ae03b53be370f056230b76343b8c75d
JulienLuxx <JulienLux@outlook.com>
2021-02-24 11:16:16 +0800

    AddPushMessageResultDto

---
+++ Common/Common.WeComCore/Dto/PushMessageResultDto.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Common.WeComCore
+{
+    public class PushMessageResultDto: WeComBaseResultDto
+    {
+        [JsonProperty("invaliduser")]
+        public string InvalidUser { get; set; }
+
+        [JsonProperty("invalidparty")]
+        public string InvalidParty { get; set; }
+
+        [JsonProperty("invalidtag")]
+        public string InvalidTag { get; set; }
+    }
+}
