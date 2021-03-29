b9bcda97109c7424cec08f0a8b5140d05737dc38
JulienLuxx <JulienLux@outlook.com>
2021-03-29 09:44:01 +0800

    AddJsApiTicketParam

---
+++ Common/Common.WeComCore/Param/JsApiTicketParam.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace Common.WeComCore
+{
+    public interface IJsApiTicketParam
+    {
+        string AccessToken { get; set; }
+    }
+
+    public class JsApiTicketEnterpriseParam: IJsApiTicketParam
+    {
+        [Description("access_token")]
+        public string AccessToken { get; set; }
+
+        public JsApiTicketEnterpriseParam() { }
+
+        public JsApiTicketEnterpriseParam(string accessToken)
+        {
+            AccessToken = accessToken;
+        }
+    }
+
+    public class JsApiTicketAppParam: JsApiTicketEnterpriseParam, IJsApiTicketParam
+    {
+        [Description("type")]
+        public string Type { get; set; }
+
+        public JsApiTicketAppParam() : base() 
+        {
+            Type = "agent_config";
+        }
+
+        public JsApiTicketAppParam(string accessToken) : base(accessToken)
+        {
+            Type = "agent_config";
+        }
+    }
+}
