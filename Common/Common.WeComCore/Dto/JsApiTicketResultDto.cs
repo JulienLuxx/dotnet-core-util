9626435e3163eb961f6bba3c44a534466cc850fc
JulienLuxx <JulienLux@outlook.com>
2021-03-29 09:44:18 +0800

    AddJsApiTicketResultDto

---
+++ Common/Common.WeComCore/Dto/JsApiTicketResultDto.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Common.WeComCore
+{
+    public class JsApiTicketResultDto : WeComBaseResultDto, IWeComResultDto
+    {
+        [JsonProperty("ticket")]
+        public string Ticket { get; set; }
+
+        [JsonProperty("expires_in")]
+        public int ExpiresIn { get; set; }
+    }
+}
