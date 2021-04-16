39c8bf72d478c7fbfaeea9c3fb08fc482f2178ef
JulienLuxx <JulienLux@outlook.com>
2021-04-16 16:52:18 +0800

    AddSignatureResultDto&AddSignatureDto

---
+++ Common/Common.WeComCore/Dto/SignatureResultDto.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Common.WeComCore
+{
+    public class SignatureResultDto : WeComBaseResultDto
+    {
+        public SignatureDto Signature { get; set; }
+    }
+
+    public class SignatureDto
+    {
+        public string AgentId { get; set; }
+
+        public string CorpId { get; set; }
+
+        public string NonceStr { get; set; }
+
+        public string Signature { get; set; }
+
+        public string Ticket { get; set; }
+
+        public long Timestamp { get; set; }
+    }
+}
