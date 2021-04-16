96f8c24537596c2f034b80df66729c14eccdd4c5
JulienLuxx <JulienLux@outlook.com>
2021-04-16 16:52:36 +0800

    AddCalculateSignatureParam

---
+++ Common/Common.WeComCore/Param/CalculateSignatureParam.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Common.WeComCore
+{
+    public class CalculateSignatureParam
+    {
+        public string AgentId { get; set; }
+
+        public string CorpId { get; set; }
+
+        public string Ticket { get; set; }
+
+        public string Url { get; set; }
+    }
+}
