b979183c8b4173615ade855a05f7c9f9250e676b
JulienLuxx <JulienLux@outlook.com>
2021-04-22 11:48:31 +0800

    AddDateTimeExtensions&AddToISO8601FormatStringFunc

---
+++ Common/Common.Util/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace Common.Util
+{
+    public static partial class DateTimeExtensions
+    {
+        public static string ToISO8601FormatString(this DateTime date)
+        {
+            return date.ToUniversalTime().ToString("yyyy-MM-dd'T'HH:mm:ss'Z'", CultureInfo.CreateSpecificCulture("en-US"));
+        }
+    }
+}
