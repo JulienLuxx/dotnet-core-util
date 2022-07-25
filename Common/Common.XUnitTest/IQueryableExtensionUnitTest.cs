635ab36fe642d177b5415adee88e2da57e996caa
JulienLuxx <JulienLux@outlook.com>
2022-07-25 10:27:26 +0800

    AddJoinStringTest

--- Common/Common.XUnitTest/IQueryableExtensionUnitTest.cs
+++ Common/Common.XUnitTest/IQueryableExtensionUnitTest.cs
@@ -1,10 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Xunit;
+using Common.Util;
 
 namespace Common.XUnitTest
 {
     public class IQueryableExtensionUnitTest: BaseUnitTest
     {
+
+        [Fact]
+        public void JoinStringTest()
+        {
+            var list = new List<string>
+            {
+                "0","9","2"
+            };
+            var str = list.JoinString();
+            Assert.Equal(5, str.Length);
+        }
     }
 }
