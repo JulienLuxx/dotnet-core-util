6d02e3c8199b6d3df93fb6d6cb96c9b89ce3b741
JulienLuxx <JulienLux@outlook.com>
2022-07-25 10:56:24 +0800

    AddServiceCollectionExtensionsFunc

---
+++ Common/Common.AliyunCSBCore/ServiceCollectionExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Common.AliyunCSBCore
+{
+    public static partial class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddSignUtil(this IServiceCollection services)
+        {
+            //if (!services.Where(x => x.ServiceType == typeof(IHttpClientFactory)).Any())
+            //{
+            //    services.AddHttpClient();
+            //}
+            services.AddSingleton<ISignUtil, SignUtil>();
+            return services;
+        }
+    }
+}
