02e4301ca8eec07b2d3988e6b2132df1e30fdee6
JulienLuxx <JulienLux@outlook.com>
2022-08-02 10:14:51 +0800

    AddPollySupport

--- Common/Common.BaiduAICore/ServiceCollectionExtensions.cs
+++ Common/Common.BaiduAICore/ServiceCollectionExtensions.cs
@@ -4,6 +4,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
 using System.Text;
+using Polly;
+using Polly.Extensions.Http;
 
 namespace Common.BaiduAICore
 {
@@ -11,11 +13,21 @@ namespace Common.BaiduAICore
     {
         public static IServiceCollection AddOcrUtil(this IServiceCollection services)
         {
-            if (!services.Where(x => x.ServiceType == typeof(IHttpClientFactory)).Any())
-            {
-                services.AddHttpClient();
-            }
+            //if (!services.Where(x => x.ServiceType == typeof(IHttpClientFactory)).Any())
+            //{                
+            //}
+            services.AddHttpClient("BaiduOcr");
             services.AddScoped<IOcrUtil, OcrUtil>();
+            return services;
+        }
+
+        public static IServiceCollection AddOcrUtilWithPolly(this IServiceCollection services, int retryCount, int seconds)
+        {
+            services
+                .AddHttpClient("BaiduOcr")
+                .AddPolicyHandler(HttpPolicyExtensions
+                .HandleTransientHttpError()
+                .WaitAndRetryAsync(retryCount, _ => TimeSpan.FromSeconds(seconds)));
             return services;
         }
     }
