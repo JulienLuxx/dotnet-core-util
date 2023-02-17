2baa5b5b5a6c53ce38a96fef6733e8b9031198bf
JulienLuxx <JulienLux@outlook.com>
2023-02-17 09:58:12 +0800

    RemoveDistinctByMethod(resolve conflicts with Systerm.Linq)

--- Common/Common.Util/Extensions/IEnumerableExtensions.cs
+++ Common/Common.Util/Extensions/IEnumerableExtensions.cs
@@ -14,17 +14,17 @@ namespace Common.Util
         /// <param name="source"></param>
         /// <param name="keySelector"></param>
         /// <returns></returns>
-        public static IEnumerable<TSource> DistinctBy<TSource, TKey>(this IEnumerable<TSource> source, Func<TSource, TKey> keySelector)
-        {
-            HashSet<TKey> seenKeys = new HashSet<TKey>();
-            foreach (TSource element in source)
-            {
-                if (seenKeys.Add(keySelector(element)))
-                {
-                    yield return element;
-                }
-            }
-        }
+        //public static IEnumerable<TSource> DistinctBy<TSource, TKey>(this IEnumerable<TSource> source, Func<TSource, TKey> keySelector)
+        //{
+        //    HashSet<TKey> seenKeys = new HashSet<TKey>();
+        //    foreach (TSource element in source)
+        //    {
+        //        if (seenKeys.Add(keySelector(element)))
+        //        {
+        //            yield return element;
+        //        }
+        //    }
+        //}
 
         public static string JoinString(this IEnumerable<string> source)
         {
