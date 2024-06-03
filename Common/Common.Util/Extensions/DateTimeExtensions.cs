f819e22e8d30b97372f12a53e9b63707f1781d74
JulienLuxx <JulienLux@outlook.com>
2024-06-04 01:29:15 +0800

    UpdateToUnixTimeStampMethod

--- Common/Common.Util/Extensions/DateTimeExtensions.cs
+++ Common/Common.Util/Extensions/DateTimeExtensions.cs
@@ -12,11 +12,28 @@ namespace Common.Util
             return date.ToUniversalTime().ToString("yyyy-MM-dd'T'HH:mm:ss'Z'", CultureInfo.CreateSpecificCulture("en-US"));
         }
 
-        public static long ToUnixTimeStamp(this DateTime dateTime)
+        /// <summary>
+        ///  Convert to second base unix time stamp
+        /// </summary>
+        /// <param name="dateTime"></param>
+        /// <returns></returns>
+        public static long ToShortUnixTimeStamp(this DateTime dateTime)
         {
-            var start = TimeZoneInfo.ConvertTime(new DateTime(1970, 1, 1), TimeZoneInfo.Local);
+            var start = TimeZoneInfo.ConvertTime(new DateTime(1970, 1, 1, 0, 0, 0), TimeZoneInfo.Local);
             long ticks = (dateTime - start.Add(new TimeSpan(8, 0, 0))).Ticks;
-            return ticks/TimeSpan.TicksPerSecond;
+            return ticks / TimeSpan.TicksPerSecond;
+        }
+
+        /// <summary>
+        ///  Convert to millisecond base unix time stamp
+        /// </summary>
+        /// <param name="dateTime"></param>
+        /// <returns></returns>
+        public static long ToLongUnixTimeStamp(this DateTime dateTime)
+        {
+            var start = TimeZoneInfo.ConvertTime(new DateTime(1970, 1, 1, 0, 0, 0), TimeZoneInfo.Local);
+            long ticks = (dateTime - start.Add(new TimeSpan(8, 0, 0))).Ticks;
+            return ticks / TimeSpan.TicksPerMillisecond;
         }
 
         public static DateTime ToDateTime(this long timestamp)
